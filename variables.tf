13577e21d54d3447efd3118a7447404ea0f0544a
FrankGBD <frank.wen@elevancehealth.com>
2024-06-07 17:40:21 -0400

    version 1.0.0

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
